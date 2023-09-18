3014c6ac5f49073b755c39ae77bf659d90a1eba4
Gringl <ezrajbird0@gmail.com>
2023-09-18 00:07:40 -0700

    triple interface test

--- touch_test.py
+++ touch_test.py
@@ -48,6 +48,8 @@ class Hyperpixel2r:
         # Based on "Python GUI in Linux frame buffer"
         # http://www.karoltomala.com/blog/?p=679
         pygame.init()
+        font = pygame.font.Font(None, 36)
+
         DISPLAY = os.getenv("DISPLAY")
         if DISPLAY:
             print("Display: {0}".format(DISPLAY))
@@ -100,37 +102,23 @@ class Hyperpixel2r:
 
     def touch(self, x, y, state):
         if state:
-            # Define the coordinates for the Play/Pause button
-            button_x, button_y = 240, 240
-            distance = math.sqrt((button_x - x)**2 + (button_y - y)**2)
-            if distance < 90:
-                self._touched = True
-                self.toggle_playback()
-
-    def toggle_playback(self):
-        # Toggle Spotify playback
-        playback = sp.current_playback()
-        if playback and playback['is_playing']:
-            sp.pause_playback()
-        else:
-            sp.start_playback()
-
-    def display_button(self):
-        # Display the Play/Pause button
-        pygame.draw.circle(self.screen, (0, 255, 0), (240, 240), 90)
-        self._updatefb()
-
-    def display_song_info(self):
+            # Check which button was pressed and act accordingly
+            if 120 < x < 200:  # Skip backwards button
+                sp.previous_track()
+            elif 200 < x < 280:  # Pause/Play button
+                playback = sp.current_playback()
+                if playback and playback['is_playing']:
+                    sp.pause_playback()
+                else:
+                    sp.start_playback()
+            elif 280 < x < 360:  # Skip forwards button
+                sp.next_track()
+    def display_controls_and_info(self):
         # Fetch current song information
         playback = sp.current_playback()
         if playback and playback['item']:
             song_name = playback['item']['name']
             artist_name = playback['item']['artists'][0]['name']
-            album_cover_url = playback['item']['album']['images'][0]['url']  # Get the URL of the album cover
-
-            # Download the album cover
-            response = requests.get(album_cover_url)
-            album_cover = pygame.image.load(io.BytesIO(response.content))
 
             # Clear the screen
             self.screen.fill((0, 0, 0))
@@ -143,42 +131,33 @@ class Hyperpixel2r:
             song_surface = font.render(song_name, True, text_color)
             artist_surface = font.render(artist_name, True, text_color)
 
-            # Position the text lower and centered
-            song_rect = song_surface.get_rect(center=(240, 320))
-            artist_rect = artist_surface.get_rect(center=(240, 360))
+            # Position the text centered and above the buttons
+            song_rect = song_surface.get_rect(center=(240, 180))
+            artist_rect = artist_surface.get_rect(center=(240, 220))
 
             # Draw the text on the screen
             self.screen.blit(song_surface, song_rect)
             self.screen.blit(artist_surface, artist_rect)
 
-            # Display the album cover in the middle and make it spin
-            # Note: This is a basic implementation. You might need to adjust for the desired effect.
-            angle = 0
-            while True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        sys.exit()
+            # Define button colors and positions
+            button_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+            button_positions = [(160, 240), (240, 240), (320, 240)]
 
-                rotated_cover = pygame.transform.rotate(album_cover, angle)
-                rect = rotated_cover.get_rect(center=(240, 240))
-                self.screen.blit(rotated_cover, rect.topleft)
-                pygame.display.flip()
-                angle += 1  # Adjust this value for faster or slower rotation
-                time.sleep(0.01)  # Adjust this value for smoother or choppier rotation
+            # Draw the buttons on the screen
+            for color, position in zip(button_colors, button_positions):
+                pygame.draw.circle(self.screen, color, position, 40)
 
             # Update the display
             self._updatefb()
+
+
 display = Hyperpixel2r()
 touch = Touch()
 
 @touch.on_touch
 def handle_touch(touch_id, x, y, state):
     display.touch(x, y, state)
-
-display.display_button()
-display.display_song_info()
-
+display.display_controls_and_info()
 running = True
 while running:
     for event in pygame.event.get():
